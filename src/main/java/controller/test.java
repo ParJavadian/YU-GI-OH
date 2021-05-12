cb2e8e2a916f39451c0d826a160abe0465f53b76
kiana-msz <kianamousazadeh2018@gmail.com>
2021-05-12 14:38:04 +0430

    loginController tests

--- src/main/java/controller/test.java
+++ src/main/java/controller/test.java
@@ -1,14 +1,21 @@
 package controller;
 
 import controller.exeption.InvalidSelection;
+import controller.exeption.RepetitiveNickname;
+import controller.exeption.RepetitiveUsername;
 import model.Deck;
 import model.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
+import view.LogInView;
 
 import javax.jws.soap.SOAPBinding;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
 
 public class test {
 
@@ -155,5 +162,45 @@ public class test {
                 duelController.selectCardOpponentTrapAndSpellZone(0);
             }
         });
+    }
+
+//    @Test
+//    @DisplayName("test menu enter")
+//    public void testMenuEnter(){
+//        InputStream sysInBackup = System.in; // backup System.in to restore it later
+//        PrintStream sysOutBackup = System.out;
+//        ByteArrayInputStream in = new ByteArrayInputStream("menu enter \nmenu exit\n".getBytes());
+//        System.setIn(in);
+//        ByteArrayOutputStream out = new ByteArrayOutputStream();
+//        System.setOut(new PrintStream(out));
+//        LogInView.getInstance().getCommandForLogin();
+//        Assertions.assertEquals("please login first\n",out.toString());
+//        System.setIn(sysInBackup);
+//        System.setOut(sysOutBackup);
+//    }
+
+    @Test
+    @DisplayName("Repetitive username check")
+    public void RepetitiveUsername() throws Exception {
+        LogInController.getInstance().createUser("kiana_msz","12345","kiana");
+        Assertions.assertThrows(RepetitiveUsername.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                LogInController.getInstance().createUser("kiana_msz","12345","kiana");
+            }
+        });
+    }
+
+    @Test
+    @DisplayName("Repetitive nickname check")
+    public void RepetitiveNickname() throws Exception {
+        LogInController.getInstance().createUser("kiana_m","12345","kiana");
+        Assertions.assertThrows(RepetitiveNickname.class, new Executable() {
+            @Override
+            public void execute() throws Throwable {
+                LogInController.getInstance().createUser("kiana_msz","12345","kiana");
+            }
+        });
+
     }
 }
