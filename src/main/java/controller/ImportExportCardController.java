4c9e2487a15ae5c9d4132d6711aa955d767414b4
Hamraz <hamriouz@gmail.com>
2021-06-16 15:11:25 +0430

    importExport user esme tabe ha

---
+++ src/main/java/controller/ImportExportCardController.java
@@ -0,0 +1,32 @@
+package controller;
+
+import model.Cardable;
+import model.User;
+
+public class ImportExportCardController {
+    private static ImportExportCardController instance = null;
+    private User user;
+
+    public static ImportExportCardController getInstance(User user) {
+        if (instance == null) instance = new ImportExportCardController(user);
+        else if(!instance.user.equals(user)) instance.user = user;
+        return instance;
+    }
+
+    private ImportExportCardController(User user){
+    this.user = user;
+    }
+
+    public void importCard(Cardable card){
+
+    }
+
+    public void exportCard(Cardable card){
+
+    }
+
+    public void importUsers(){
+
+    }
+
+}
