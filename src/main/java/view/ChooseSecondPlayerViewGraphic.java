220374f5ca1c5d3878277dc28f8a08345a80e022
ParJavadian <parjavadian@gmail.com>
2021-07-06 12:11:46 +0430

    ChooseSecondPlayer combobox

--- src/main/java/view/ChooseSecondPlayerViewGraphic.java
+++ src/main/java/view/ChooseSecondPlayerViewGraphic.java
@@ -1,24 +1,25 @@
 package view;
 
 import javafx.application.Application;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import model.User;
 
-import java.lang.reflect.Array;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class ChooseSecondPlayerViewGraphic extends Application {
+public class ChooseSecondPlayerViewGraphic extends Application implements Initializable {
     private static Stage stage;
     static ChooseSecondPlayerViewGraphic instance = null;
     private static User user;
-    public ComboBox<String> choiceBox;
+    public ComboBox<String> comboBox = new ComboBox<>();
 
     public static ChooseSecondPlayerViewGraphic getInstance() {
         if (instance == null) instance = new ChooseSecondPlayerViewGraphic();
@@ -40,29 +41,35 @@ public class ChooseSecondPlayerViewGraphic extends Application {
     }
 
 
-    public void onPlayerGame(MouseEvent event) throws Exception{
+    public void onPlayerGame(MouseEvent event) throws Exception {
         ChooseDuelView.getInstance().setCurrentUser(user);
         ChooseDuelView.getInstance().setPlayerTwoName("do");
         ChooseDuelView.getInstance().start(stage);
     }
 
-    public void playWithAi(MouseEvent event) throws Exception{
+    public void playWithAi(MouseEvent event) throws Exception {
         ChooseDuelView.getInstance().setCurrentUser(user);
         ChooseDuelView.getInstance().setPlayerTwoName("@AI@");
         ChooseDuelView.getInstance().start(stage);
     }
 
-    public void goBack(MouseEvent event) throws Exception{
+    public void goBack(MouseEvent event) throws Exception {
         MainViewGraphic.getInstance().start(stage);
     }
 
-    public void fillTheComboBoxWithUsers(){
+    public void fillTheComboBoxWithUsers() {
         ArrayList<String> choices = new ArrayList<>();
-        for (User user:User.getAllUsers()) {
+        for (User user : User.getAllUsers()) {
             String username = user.getUsername();
-            if (!username.equals(ChooseSecondPlayerViewGraphic.user.getUsername()))
+            if (!username.equals(ChooseSecondPlayerViewGraphic.user.getUsername())) {
                 choices.add(username);
+            }
         }
-        choiceBox.setItems((ObservableList<String>) choices);
+        comboBox.getItems().addAll(choices);
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        fillTheComboBoxWithUsers();
     }
 }
