f180c50832de2455747b0b9b3a886fde46d5ab98
ParJavadian <parjavadian@gmail.com>
2021-07-10 05:22:35 +0430

    FAZE 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import controller.SoundController;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import view.SignUpViewGraphic;
@@ -11,6 +12,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        SoundController.getInstance().playWhenStart();
         SignUpViewGraphic.getInstance().start(primaryStage);
     }
 }
