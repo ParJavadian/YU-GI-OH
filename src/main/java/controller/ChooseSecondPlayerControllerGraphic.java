cbed2cf3c44be88fc5900be81fac11e07f4e64d4
Hamraz <hamriouz@gmail.com>
2021-07-18 12:16:58 +0430

    remove combo box

--- src/main/java/controller/ChooseSecondPlayerControllerGraphic.java
+++ src/main/java/controller/ChooseSecondPlayerControllerGraphic.java
@@ -9,10 +9,10 @@ import view.ChooseDuelView;
 
 public class ChooseSecondPlayerControllerGraphic {
 
-    public static void twoPlayerGame(User user, Stage stage)throws Exception {
+    public static void twoPlayerGame(User user, Stage stage) throws Exception {
         if (user.getActiveDeck() == null)
             throw new NoActiveDeck(user.getUsername());
-        if(!user.getActiveDeck().isValid())
+        if (!user.getActiveDeck().isValid())
             throw new InvalidDeck(user.getUsername());
         ChooseDuelView.getInstance().setCurrentUser(user);
         ChooseDuelView.getInstance().setPlayerTwoName("");
--- src/main/java/view/ChooseSecondPlayerViewGraphic.java
+++ src/main/java/view/ChooseSecondPlayerViewGraphic.java
@@ -15,11 +15,10 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
-public class ChooseSecondPlayerViewGraphic extends Application implements Initializable {
+public class ChooseSecondPlayerViewGraphic extends Application {
     private static Stage stage;
     static ChooseSecondPlayerViewGraphic instance = null;
     private static User user;
-    public ComboBox<String> comboBox = new ComboBox<>();
 
     public static ChooseSecondPlayerViewGraphic getInstance() {
         if (instance == null) instance = new ChooseSecondPlayerViewGraphic();
@@ -31,27 +30,21 @@ public class ChooseSecondPlayerViewGraphic extends Application implements Initia
     }
 
     @Override
-    public void initialize(URL location, ResourceBundle resources) {
-        fillTheComboBoxWithUsers();
-    }
-
-    @Override
     public void start(Stage primaryStage) throws Exception {
         ChooseSecondPlayerViewGraphic.stage = primaryStage;
         URL url = getClass().getResource("/ChooseSecondPlayer.fxml");
         Parent root = FXMLLoader.load(url);
         Scene scene = new Scene(root);
         stage.setScene(scene);
-        fillTheComboBoxWithUsers();
         stage.show();
     }
 
     public void twoPlayerGame(){
         try {
-            ChooseSecondPlayerControllerGraphic.twoPlayerGame(comboBox.getValue(),user,stage);
+            ChooseSecondPlayerControllerGraphic.twoPlayerGame(user,stage);
         }
         catch (Exception e){
-            printError(e.getMessage());
+            e.printStackTrace();
         }
     }
 
@@ -65,22 +58,6 @@ public class ChooseSecondPlayerViewGraphic extends Application implements Initia
         MainViewGraphic.getInstance().start(stage);
     }
 
-    public void fillTheComboBoxWithUsers() {
-        ArrayList<String> choices = new ArrayList<>();
-        for (User user : User.getAllUsers()) {
-            String username = user.getUsername();
-            if (!username.equals(ChooseSecondPlayerViewGraphic.user.getUsername()) && !username.equals("@AI@")) {
-                choices.add(username);
-            }
-        }
-        comboBox.getItems().addAll(choices);
-    }
 
-    private void printError(String command) {
-        Alert alert = new Alert(Alert.AlertType.ERROR, command);
-        alert.setHeaderText("");
-        alert.setTitle("");
-        alert.showAndWait();
-    }
 
 }
